1b27678bc0a48e122228646d7a7d9b0a54add4f1
Almas Mendygaliyev <MendygaliyevA@twd.com>
2018-03-12 12:18:20 -0400

    initial commit

---
+++ ReactClock/app.js
@@ -0,0 +1,4 @@
+setInterval(() =>{
+    const myClock = React.createElement("h1", null, "The time is: " + new Date().toLocaleTimeString())
+    ReactDOM.render(myClock, document.getElementById("app"))
+}, 1000)
